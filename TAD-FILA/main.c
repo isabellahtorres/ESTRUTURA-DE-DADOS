caf5d6fad765c3133d291568de237e15ab78a3c6
Isabella Torres <87441754+isabellahtorres@users.noreply.github.com>
2023-07-27 19:40:45 -0300

    Update main.c

--- TAD-FILA/main.c
+++ TAD-FILA/main.c
@@ -2,6 +2,49 @@
 #include "fila.h"
 
 int main() {
-	imprimeMenu();
+    int whileBreak = 0;
+    int elemento;
+    Fila f = inicializar();
+    while (whileBreak == 0) {
+        printf("\nEscolha uma das opções abaixo:\n");
+        printf("1 - Inserir na fila\n");
+        printf("2 - Remover da fila\n");
+        printf("3 - Imprimir a fila\n");
+        printf("4 - Sair\n");
+
+        int opcaoSelecionada;
+        scanf("%d", &opcaoSelecionada);
+
+        switch (opcaoSelecionada) {
+            case 1:
+                if (cheia(f))
+					printf("Fila cheia!!\n");
+				else {
+					printf("Digite o elemento a ser inserido: ");
+					scanf("%d", &elemento);
+					push(f, elemento);
+				}
+                break;
+
+            case 2:{
+				int x;
+				if (vazia(f))
+					printf("Fila vazia!!\n");
+				else if (pop(f, &x))
+					printf("O item removido foi: %d\n", x);
+				break;
+			}
+            case 3:
+                imprimir(f);
+                break;
+
+            case 4:{
+				destruir(f);
+                whileBreak = 1;
+              break;
+            }
+        }
+    }
+}
 	return 0;
 }
